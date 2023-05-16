ad80cb6c553bc2aa122d01373aef6c35a7df4140
Lenka Černohorská <cernohorska.lc@gmail.com>
2023-05-16 22:55:53 +0200

    Opravena komponenta Email

--- EmailSection/index.js
+++ EmailSection/index.js
@@ -2,16 +2,31 @@
 import { Email } from '../Email/index.js'
 
 export const EmailSection = (props) => {
-    const { heading, emails, folder } = props
+    const { heading, folder, emailResult } = props
 
     const element = document.createElement('section')
     element.classList.add('inbox')
     element.innerHTML = `
-        <h2>${heading}</h2>
-        <div class="emails" id=${folder}></div>
-    `
+            <h2>${heading}</h2>
+            <div class="emails" id=${folder}></div>
+        `
 
-    const listEmails = emails.map(oneEmail => Email(oneEmail))
+    if (emailResult === 'loading') {
+        fetch(`https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=${folder}`)
+            .then((response) => response.json())
+            .then((data) => {
+                element.replaceWith(
+                    EmailSection({
+                        heading: heading,
+                        emailResult: data.emails,
+                        folder: folder,
+                    })
+                )
+            })
+        return element
+    }
+
+    const listEmails = emailResult.map(oneEmail => Email(oneEmail))
     element.querySelector('.emails').append(...listEmails)
 
     return element
--- script.js
+++ script.js
@@ -4,24 +4,7 @@ import { EmailSection } from './EmailSection/index.js'
 
 const appElm = document.querySelector('#app')
 
-fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=unread')
-    .then((response) => response.json())
-    .then((data) => {
-        appElm.append(EmailSection({
-            heading: 'Nepřečtené',
-            emails: data.emails,
-            folder: 'unread',
-        }))
-    })
-
-fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=read')
-    .then((response) => response.json())
-    .then((data) => {
-        appElm.append(EmailSection({
-            heading: 'Přečtené',
-            emails: data.emails,
-            folder: 'read',
-        }))
-    })
-
-
+appElm.append(
+    EmailSection({ heading: 'Nepřečtené', folder: 'unread', emailResult: 'loading' }),
+    EmailSection({ heading: 'Přečtené', folder: 'read', emailResult: 'loading' })
+)
